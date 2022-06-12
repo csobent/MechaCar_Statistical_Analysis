27bc089dd31c017e1baca05a172c13face59148c
Carter <98721891+csobent@users.noreply.github.com>
2022-06-12 14:22:43 -0400

    Update MechaCarChallenge.R

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -1,5 +1,4 @@
 > MechaCar_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-> View(MechaCar_data)
 > lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data)
 
 Call:
@@ -24,13 +23,13 @@ Residuals:
 -19.4701  -4.4994  -0.0692   5.4433  18.5849 
 
 Coefficients:
-  Estimate Std. Error t value Pr(>|t|)    
+                  Estimate  Std. Error  t value Pr(>|t|)    
 (Intercept)      -1.040e+02  1.585e+01  -6.559 5.08e-08 ***
-  vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
-  vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
+vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
+vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
 spoiler_angle     6.877e-02  6.653e-02   1.034   0.3069    
 ground_clearance  3.546e+00  5.412e-01   6.551 5.21e-08 ***
-  AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
+AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
 ---
   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
@@ -41,7 +40,4 @@ F-statistic: 22.07 on 5 and 44 DF,  p-value: 5.35e-11
 > setwd("~/Desktop/R_Analysis/Challenge ")
 > suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 > total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
-> View(total_summary)
-> View(suspension_coil)
 > lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
-> View(lot_summary)
